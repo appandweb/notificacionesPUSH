09ec204be0c6f3e77c8a5c87fef1faea9da10f5f
App&Web <appandweb@appandweb.es>
2014-04-08 12:48:44 +0200

    Version 1.0

---
+++ notificacionesPUSH/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  notificacionesPUSH
+//
+//  Created by Ruben on 07/04/14.
+//  Copyright (c) 2014 AppAndWeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
